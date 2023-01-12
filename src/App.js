b98893ad4cc74d90c00825c8b14f4fd2ddd66530
John <jrc2855@gmail.com>
2023-01-12 00:50:08 +0000

    Modal Running on Lab 03

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Main from './Main';
 import Header from './Header';
 import Footer from './Footer';
 import data from './data.json';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/Main.js
+++ src/Main.js
@@ -2,8 +2,29 @@ import React, { Component } from 'react'
 import HornedBeast from './HornedBeast'
 import data from './data.json'
 import './Main.css'
+import SelectedBeast from './SelectedBeast'
 
 export default class Main extends Component {
+
+  constructor(props){
+    super(props);
+      this.state = {
+        showModal:false,
+      }
+  }
+
+handleOpenModal = () => {
+  this.setState({
+    showModal:true,
+  });
+}
+
+handleCloseModal = () => {
+  this.setState({
+    showModal: false
+  })
+}
+
   render() {
     return (
       <div>
@@ -11,12 +32,10 @@ export default class Main extends Component {
         <p>John</p>
         <main>
          {data.map((beastObj) => {
-           return <HornedBeast title={beastObj.title} imageURL={beastObj.image_url} alt={beastObj.keyword} description={beastObj.description}/>
+           return <HornedBeast title={beastObj.title} imageURL={beastObj.image_url} alt={beastObj.keyword} description={beastObj.description} handleOpenModal={this.handleOpenModal}/>
          })}
           </main>  
-        {/* <HornedBeast title="Uniwhal" description="A unicorn and a narwhal nuzzling their horns" imageURL="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
-
-        <HornedBeast title="Rhino" description="Parent rhino with two babies" imageURL="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"/> */}
+          <SelectedBeast Show={this.state.showModal} handleClose={this.handleCloseModal}/>
         </div>
     )
   }
